98a09cf1b099ceebc0dde257165387d94406e15d
Jarek Sacha <jpsacha@gmail.com>
2016-06-17 23:27:34 -0400

    Update copyright notice with new email and project location.

--- scala-console/src/main/scala/net/sf/ij_plugins/scala/console/ScalaConsoleFrame.scala
+++ scala-console/src/main/scala/net/sf/ij_plugins/scala/console/ScalaConsoleFrame.scala
@@ -1,33 +1,35 @@
 /*
- * Image/J Plugins
- * Copyright (C) 2002-2014 Jarek Sacha
- * Author's email: jsacha at users dot sourceforge dot net
+ *  ImageJ Plugins
+ *  Copyright (C) 2002-2016 Jarek Sacha
+ *  Author's email: jpsacha at gmail dot com
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Latest release available at http://sourceforge.net/projects/ij-plugins/
+ *   Latest release available at https://github.com/ij-plugins
  */
 
 package net.sf.ij_plugins.scala.console
 
 import java.awt.Cursor
+
 import net.sf.ij_plugins.scala.console
 import net.sf.ij_plugins.scala.console.ScalaInterpreter._
 import net.sf.ij_plugins.scala.console.editor.Editor
 import net.sf.ij_plugins.scala.console.editor.Editor.SourceFileEvent
 import net.sf.ij_plugins.scala.swing.ToolBar
+
 import scala.swing.BorderPanel._
 import scala.swing.Component._
 import scala.swing._
@@ -37,7 +39,8 @@ import scala.tools.nsc.interpreter.Results
 
 /**
  * Main view for the Scala Console.
- * @author Jarek Sacha
+  *
+  * @author Jarek Sacha
  * @since 2/11/12
  */
 private class ScalaConsoleFrame(val editor: Editor,
