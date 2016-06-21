1fa91dc9c528b3f908019e200c6abc86575dab41
Jarek Sacha <jpsacha@gmail.com>
2016-06-21 09:19:56 -0400

    Code style correction.

--- scala-console/src/main/scala/net/sf/ij_plugins/scala/console/ScalaConsoleFrame.scala
+++ scala-console/src/main/scala/net/sf/ij_plugins/scala/console/ScalaConsoleFrame.scala
@@ -38,102 +38,102 @@ import scala.tools.nsc.interpreter.Results
 
 
 /**
- * Main view for the Scala Console.
+  * Main view for the Scala Console.
   *
   * @author Jarek Sacha
- * @since 2/11/12
- */
+  * @since 2/11/12
+  */
 private class ScalaConsoleFrame(val editor: Editor,
                                 val controller: ScalaConsoleController,
                                 val model: ScalaInterpreter) extends Frame {
 
-    private val defaultTitle = "Scala Console"
-    title = defaultTitle
+  private val defaultTitle = "Scala Console"
+  title = defaultTitle
 
-    // Set frame icons, at different scales.
-    peer.setIconImages({
-        val names = Array("scala16.png", "scala32.png", "scala48.png", "scala64.png")
-        val icons = for (name <- names) yield console.loadImage(this.getClass, "resources/" + name)
-      import scala.collection.JavaConversions._
-      icons.toList
-    })
+  // Set frame icons, at different scales.
+  peer.setIconImages({
+    val names = Array("scala16.png", "scala32.png", "scala48.png", "scala64.png")
+    val icons = for (name <- names) yield console.loadImage(this.getClass, "resources/" + name)
+    import scala.collection.JavaConversions._
+    icons.toList
+  })
 
 
-    // Create local actions
-    private val runAction = new Action("Run") {
-        icon = console.loadIcon(this.getClass, "resources/icons/script_go.png")
+  // Create local actions
+  private val runAction = new Action("Run") {
+    icon = console.loadIcon(this.getClass, "resources/icons/script_go.png")
 
-        def apply() {
-            controller.run()
-        }
+    def apply() {
+      controller.run()
+    }
+  }
+
+  // Setup menu bar
+  menuBar = new MenuBar {
+    contents += new Menu("File") {
+      contents ++= editor.fileActions.map(new MenuItem(_))
+
+      contents += new Separator()
+
+      contents += new MenuItem(Action("Exit") {
+        controller.exit()
+      })
+    }
+    contents += new Menu("Script") {
+      contents += new MenuItem(runAction)
     }
 
-    // Setup menu bar
-    menuBar = new MenuBar {
-        contents += new Menu("File") {
-            contents ++= editor.fileActions.map(new MenuItem(_))
+  }
 
-            contents += new Separator()
+  // Setup tool bar
+  val toolBar = new ToolBar
+  // Add editor actions that have icons
+  val fileToolActions = editor.fileActions.filter(a => a.icon != null && a.icon != Swing.EmptyIcon)
+  fileToolActions.foreach(toolBar += _)
+  if (fileToolActions.nonEmpty)
+    toolBar.addSeparator()
+  // Add script run
+  toolBar += runAction
 
-            contents += new MenuItem(Action("Exit") {
-                controller.exit()
-            })
-        }
-        contents += new Menu("Script") {
-            contents += new MenuItem(runAction)
-        }
 
+  // Position components
+  val outputArea = new OutputArea()
+  val statusLine = new Label("Welcome to Scala Console")
+  contents = new BorderPanel {
+    add(toolBar, Position.North)
+    add(new SplitPane(Orientation.Horizontal, editor.view, wrap(outputArea)), Position.Center)
+    add(statusLine, Position.South)
+  }
+
+
+  private val enablable = Array(runAction)
+
+  // Listen to models
+  listenTo(model, editor)
+
+  // React to models and internal changes
+  reactions += {
+    case StateEvent(state) =>
+      val isReady = state == State.Ready
+      enablable.foreach(_.enabled = isReady)
+      cursor = if (isReady) Cursor.getDefaultCursor else Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)
+      statusLine.text = state.toString
+
+    case ErrStreamEvent(data) => outputArea.appendErrStream(data)
+    case OutStreamEvent(data) => outputArea.appendOutStream(data)
+    case InterpreterLogEvent(data) => outputArea.appendInterpreterOut(data)
+    case ResultEvent(result) =>
+      result match {
+        case Results.Error => outputArea.appendErrStream(result.toString)
+        case Results.Success => outputArea.appendOutStream(result.toString)
+        case Results.Incomplete => outputArea.appendErrStream(result.toString)
+      }
+
+    case SourceFileEvent(fileOption) => fileOption match {
+      case Some(file) => title = defaultTitle + " - " + file.getCanonicalPath
+      case None => title = defaultTitle
     }
 
-    // Setup tool bar
-    val toolBar         = new ToolBar
-    // Add editor actions that have icons
-    val fileToolActions = editor.fileActions.filter(a => a.icon != null && a.icon != Swing.EmptyIcon)
-    fileToolActions.foreach(toolBar += _)
-    if (!fileToolActions.isEmpty)
-        toolBar.addSeparator()
-    // Add script run
-    toolBar += runAction
-
-
-    // Position components
-    val outputArea = new OutputArea()
-    val statusLine = new Label("Welcome to Scala Console")
-    contents = new BorderPanel {
-        add(toolBar, Position.North)
-        add(new SplitPane(Orientation.Horizontal, editor.view, wrap(outputArea)), Position.Center)
-        add(statusLine, Position.South)
-    }
-
-
-    private val enablable = Array(runAction)
-
-    // Listen to models
-    listenTo(model, editor)
-
-    // React to models and internal changes
-    reactions += {
-      case StateEvent(state) =>
-        val isReady = state == State.Ready
-        enablable.foreach(_.enabled = isReady)
-        cursor = if (isReady) Cursor.getDefaultCursor else Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)
-        statusLine.text = state.toString
-
-      case ErrStreamEvent(data) => outputArea.appendErrStream(data)
-      case OutStreamEvent(data) => outputArea.appendOutStream(data)
-        case InterpreterLogEvent(data) => outputArea.appendInterpreterOut(data)
-      case ResultEvent(result) =>
-        result match {
-          case Results.Error => outputArea.appendErrStream(result.toString)
-          case Results.Success => outputArea.appendOutStream(result.toString)
-          case Results.Incomplete => outputArea.appendErrStream(result.toString)
-        }
-
-      case SourceFileEvent(fileOption) => fileOption match {
-        case Some(file) => title = defaultTitle + " - " + file.getCanonicalPath
-            case None => title = defaultTitle
-        }
-
-        case WindowClosing(_) => editor.prepareToClose()
-    }
+    case WindowClosing(_) => editor.prepareToClose()
+  }
 }
